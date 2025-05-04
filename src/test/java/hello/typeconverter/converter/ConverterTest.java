8313a637463de42c40e3ed3fdd4b4d61fcf86534
sangtaek <staek1533@gmail.com>
2025-05-05 01:15:54 +0900

    타입 컨버터 테스트

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        Assertions.assertThat(result).isEqualTo(10);
+
+    }
+
+    @Test
+    void IntegerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        Assertions.assertThat(result).isEqualTo("10");
+    }
+
+}
