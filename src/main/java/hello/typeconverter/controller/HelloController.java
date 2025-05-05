e24244a90cea192124ebe9ddc0deca8c97fec54e
sangtaek <staek1533@gmail.com>
2025-05-05 17:08:29 +0900

    Formatter study

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+
+}
