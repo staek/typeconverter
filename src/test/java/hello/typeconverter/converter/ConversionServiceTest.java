08038e9d3dd7efc683c826d007e484c4ec6322d9
sangtaek <staek1533@gmail.com>
2025-05-05 13:06:08 +0900

    ConversionService 스터디

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+public class ConversionServiceTest {
+    
+    @Test
+    void conversionService() {
+        
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+        
+        Assertions.assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+
+
+
+    }
+}
